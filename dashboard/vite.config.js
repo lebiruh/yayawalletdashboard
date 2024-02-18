76017fce3482c2788107044cac0d92026c22a9ad
Biruh <lebiruh@yahoo.com>
2024-02-18 12:19:36 +0300

    added server

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    // host: true,
+    host: true,
   //   port: 80,
     proxy: {
       '/api': 'https://yayawallet.com'
